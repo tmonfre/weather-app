278159f98d08893da9721ca6a1f0c1f2a749a535
Thomas Monfre <tmonfre@users.noreply.github.com>
2019-01-28 23:42:36 -0500

    put api key in .env

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ class NavBar extends Component {
                 <div id="button-area">
                     <a href="http://tmonfre.surge.sh" target="_blank" rel="noopener noreferrer"><h3 id="reset-button">ABOUT ME</h3></a>
                     <a href="https://github.com/tmonfre" target="_blank" rel="noopener noreferrer"><img src={require("../assets/github.svg")} alt="GitHub logo"></img></a>
-                    <a href="https://www.linkedin.com/in/thomas-monfre-0b1a50164/" target="_blank" rel="noopener noreferrer"><img src={require("../assets/linkedin.png")} alt="LinkedIn logo"></img></a>
+                    <a href="https://www.linkedin.com/in/thomas-monfre/" target="_blank" rel="noopener noreferrer"><img src={require("../assets/linkedin.png")} alt="LinkedIn logo"></img></a>
                 </div>
                 <div style={{clear: "both"}}></div>
 			</div>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+require('dotenv').config() // load environment variables
 
-// this file performs an HTTP GET request and passes the loaded data to the App component using ReactJS
-const API_KEY = "bb70572a70f792c2983109cb5339ef07";
-ReactDOM.render(<App api_key={API_KEY} />, document.getElementById('root'));
+// render app
+ReactDOM.render(<App api_key={process.env.REACT_APP_API_KEY} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
